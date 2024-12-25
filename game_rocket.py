7bc5e6ad8ab23ca53aa7490f69c78958d54e3047
manojgodara09 <79741518+manojgodara09@users.noreply.github.com>
2024-12-25 13:28:30 +0530

    Update game_rocket.py

--- game_rocket.py
+++ game_rocket.py
@@ -6,7 +6,7 @@ import random
 from datetime import datetime
 
 
-# Configuration
+# Configuration env
 DATABASE_URL = "postgresql://fun_game_user:xOuZ6LLpXDxBGg9WnSRalfc1H1dRAqj6@dpg-ctgsn00gph6c73ckd45g-a.singapore-postgres.render.com/fun_game"
 SECRET_KEY="d6A9bE3cAf2D6E5d8eFb6d8A6Bc9D1d5F7A2"
 ALGORITHM="HS256"
