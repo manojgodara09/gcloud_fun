0cc197af05bc2612fd62a7b93ced9797c383e791
manojgodara09 <79741518+manojgodara09@users.noreply.github.com>
2024-12-28 17:17:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from login import router as login_router
+from whell import router as wheel_router
 from game_rocket import router as rocket_router
 from dice import router as dice_router
 from limbo import router as limbo_router
@@ -16,6 +17,7 @@ app.include_router(login_router)
 app.include_router(rocket_router)
 app.include_router(dice_router)
 app.include_router(limbo_router)
+app.include_router(wheel_router)
 @app.get("/")
 def root():
     return {"message": "Welcome to the Casino Backend"}
